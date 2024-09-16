87fb0cdeabedd44fe78282aa06575d960de1b9bb
danieltandem <tandem.danielvargas@gmail.es>
2024-09-16 11:55:59 +0200

    cambucha 1

--- src/components/buscador/buscador2.js
+++ src/components/buscador/buscador2.js
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import './buscador.css';
-
 const Buscador2 = ({ onSearch }) => {
   const [query, setQuery] = useState(''); // Búsqueda
   const [results, setResults] = useState([]); // Resultados devueltos del backend
@@ -33,15 +30,13 @@ const Buscador2 = ({ onSearch }) => {
   // Manejar la actualización del input de búsqueda
   const handleSearch = (event) => {
     setQuery(event.target.value); // Actualizar el valor de búsqueda
-    onSearch(event.target.value);
+    onSearch(event.target.value); // Pasa la búsqueda al componente padre
   };
 
-  // Cuando seleccionamos un QR, se redirige y se resalta
+  // Cuando seleccionamos un QR, lo marcamos como seleccionado
   const handleItemClick = (qr) => {
-    setSelectedQr(qr.id); // Resaltamos el QR seleccionado
-
-    // Redirigir a la URL del QR seleccionado. Cambia la URL según tus necesidades.
-    window.location.href = `/detalle-qr/${qr.id}`;
+    setSelectedQr(qr.id); // Resaltar el QR seleccionado
+    onSearch(qr.name_qr);  // Envía el nombre seleccionado al componente padre
   };
 
   return (
@@ -64,7 +59,7 @@ const Buscador2 = ({ onSearch }) => {
               <li
                 key={qr.id}
                 className={`buscador-item ${selectedQr === qr.id ? 'selected' : ''}`} // Resaltar el QR seleccionado
-                onClick={() => handleItemClick(qr)} // Redirigir al hacer clic
+                onClick={() => handleItemClick(qr)} // Resaltar y filtrar al hacer clic
               >
                 {qr.name_qr}
               </li>
